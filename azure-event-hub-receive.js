86c529d1146db711d6b272cfa7625ceb9c39bfb8
Jan Tielens <jtielens@microsoft.com>
2018-02-05 19:12:53 +0100

    initial version

---
+++ azure-event-hub-receive.js
@@ -0,0 +1,60 @@
+var EventHubClient = require('azure-event-hubs').Client;
+
+module.exports = function (RED) {
+    function AzureEventHubReceiveNode(config) {
+        RED.nodes.createNode(this, config);
+
+        var node = this;
+        node.connectionstring = config.connectionstring;
+        node.consumergroup = config.consumergroup;
+
+        // clear status, might be left over after updating settings
+        node.status({});
+
+        try {
+            var client = EventHubClient.fromConnectionString(node.connectionstring);
+            client.open()
+                .then(client.getPartitionIds.bind(client))
+                .then(function (partitionIds) {
+                    return partitionIds.map(function (partitionId) {
+                        // loop over all partitions of the Event Hub
+                        return client.createReceiver(config.consumergroup, partitionId, { 'startAfterTime': Date.now() }).then(function (receiver) {
+                            // succesfully connected to a partition
+                            node.log('Created partition receiver: ' + partitionId);
+                            node.status({ fill: "green", shape: "ring", text: "connected" });
+                            
+                            receiver.on('errorReceived', function (err) {
+                                node.status({ fill: "yellow", shape: "ring", text: "error received: " + err.message });
+                                this.error(err.message);
+                            });
+
+                            receiver.on('message', function (receivedMessage) {
+                                // message received from Event Hub partition
+                                var msg = { payload: receivedMessage.body }
+                                node.send(msg);
+                            });
+                        });
+                    });
+                })
+                .catch(function (err) {
+                    node.status({ fill: "red", shape: "ring", text: "unexpected error: " + err.message });
+                    this.error(err.message);
+                });
+
+            this.on('close', function (done) {
+                node.log('closing ...');
+                client.close().done(function () {
+                    node.log('closing done.');
+                    done();
+                });
+            });
+        }
+        catch (err) {
+            this.error(err.message);
+            node.status({ fill: "red", shape: "ring", text: "can't connect, " + err.message });
+        }
+    }
+
+    RED.nodes.registerType("azure-event-hub-receive", AzureEventHubReceiveNode);
+}
+
